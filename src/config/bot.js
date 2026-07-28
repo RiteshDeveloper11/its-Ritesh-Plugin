fa8b4d0720b5fb7f0b267bae8d151c8eff623205
RiteshDeveloper <riteshgupta2762@gmail.com>
2026-07-28 18:30:48 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "👋🏻 Welcome its, Ritesh Plugin",     // this is what people actually see
+        state: "👋🏻 Welcome to its, Ritesh Plugin",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
